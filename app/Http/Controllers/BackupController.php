e5f7c1a58376e7c50b69fc2f1a7bc9be96ebd43e
sharafdeveloper <m.sulemansharaf1@gmail.com>
2026-08-04 19:39:19 +0500

    another testing change

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -138,9 +138,22 @@ public function restoreExecute(Request $request)
 
     public function database()
     {
+        // $file = $this->backupService->databaseBackup();
+
+        // return response()->download($file)->deleteFileAfterSend(false);
+
+         try {
         $file = $this->backupService->databaseBackup();
 
         return response()->download($file)->deleteFileAfterSend(false);
+
+    } catch (\Throwable $e) {
+        dd([
+            'message' => $e->getMessage(),
+            'file'    => $e->getFile(),
+            'line'    => $e->getLine(),
+        ]);
+    }
     }
 
 
